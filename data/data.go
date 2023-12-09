cd82ad7aede389b3a73823014dd80074388ea485
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-12-09 12:49:48 +0100

    Tweak word string representation

--- data/data.go
+++ data/data.go
@@ -1,12 +1,10 @@
 package data
 
-import "fmt"
-
 type Word struct {
 	OriginFile string
 	Word       string
 }
 
 func (w Word) String() string {
-	return fmt.Sprintf("%s\t%s", w.OriginFile, w.Word)
+	return w.Word
 }
