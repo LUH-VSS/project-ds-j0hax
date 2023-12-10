d18647fbc65b88a5d1554a7f4ac5da6b10c264b8
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-12-10 13:28:23 +0100

    Improve subcommand handling and usage text

--- main.go
+++ main.go
@@ -9,18 +9,29 @@ import (
 	"github.com/LUH-VSS/project-ds-j0hax/reducer"
 )
 
+func subCmdError() {
+	outfile := flag.CommandLine.Output()
+	fmt.Fprintln(outfile, "Error: must use either `map` or `reduce` subcommand")
+	os.Exit(1)
+}
+
 func main() {
 
 	readerCmd := flag.NewFlagSet("map", flag.ExitOnError)
 	readerHost := readerCmd.String("host", "http://127.0.0.1:1831/words", "The host to send trend data to")
+	readerCmd.Usage = func() {
+		outfile := flag.CommandLine.Output()
+		fmt.Fprintf(outfile, "Usage: %s [OPTION] [FILE]...\n", os.Args[0])
+		fmt.Fprintln(outfile, "Extract words from FILE(s) and send these to HOST.")
+		readerCmd.PrintDefaults()
+	}
 
 	writerCmd := flag.NewFlagSet("reduce", flag.ExitOnError)
-	writerAddr := readerCmd.String("addr", ":1831", "The address to bind to")
-	writerPattern := readerCmd.String("pattern", "/words", "The pattern to listen to")
+	writerAddr := writerCmd.String("addr", ":1831", "The address to bind to")
+	writerPattern := writerCmd.String("pattern", "/words", "The pattern to listen to")
 
-	if len(os.Args) < 2 {
-		fmt.Println("expected 'reader' or 'writer' subcommands")
-		os.Exit(1)
+	if len(os.Args) <= 1 {
+		subCmdError()
 	}
 
 	switch os.Args[1] {
@@ -32,5 +43,7 @@ func main() {
 		writerCmd.Parse(os.Args[2:])
 		w := reducer.NewWriter(*writerAddr, *writerPattern, readerCmd.Arg(0))
 		w.Run()
+	default:
+		subCmdError()
 	}
 }
