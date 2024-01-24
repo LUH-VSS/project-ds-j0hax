053e0abd5a91eb051961e69ffa5d8fb291edc00c
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2024-01-24 10:32:56 +0100

    Implement batching

--- mapper/reader.go
+++ mapper/reader.go
@@ -19,8 +19,10 @@ var notLetters = regexp.MustCompile(`[^\p{L}]*`)
 
 // Reader reads the specified files in parallel and uploads the JSON-encoded words to the endpoint
 type Reader struct {
-	files    []string
-	endpoint *url.URL
+	files     []string
+	endpoint  *url.URL
+	wordQueue chan string
+	done      chan bool
 }
 
 // ProcessFile reads the file at the path in string and uploads the list to the endpoint
@@ -32,22 +34,19 @@ func (r *Reader) ProcessFile(file string) {
 	}
 	defer f.Close()
 
-	var wordList []string
-
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
 		rawWord := scanner.Text()
 		word := notLetters.ReplaceAllString(rawWord, "") // remove punctuation
-		if len(word) > 0 {
-			wordList = append(wordList, word)
-		}
+
+		// add word to program's queue
+		r.wordQueue <- word
 	}
 	if err := scanner.Err(); err != nil {
 		log.Panic(err)
 	}
 	log.Printf("Finished reading %s\n", file)
-	r.postWords(wordList)
 }
 
 // postWords sends a slice of words to the endpoint in JSON format.
@@ -55,8 +54,6 @@ func (r *Reader) ProcessFile(file string) {
 // This function blocks until the bundle is successfully sent.
 // In case of error, linear backoff is used.
 func (r *Reader) postWords(words []string) {
-	log.Printf("Sending array of %d words\n", len(words))
-
 	payload, err := json.Marshal(words)
 
 	if err != nil {
@@ -82,14 +79,38 @@ func (r *Reader) postWords(words []string) {
 	}
 }
 
+// collectWords collects words sent over the readers internal channel.
+//
+// Once batchSize is reached, the list of words is sent to the reducer.
+func (r *Reader) collectWords(batchSize int) {
+	batch := make([]string, 0, batchSize)
+	for word := range r.wordQueue {
+		batch = append(batch, word)
+
+		// Send all words in the batch once the slice is full
+		if len(batch) == cap(batch) {
+			r.postWords(batch)
+			batch = make([]string, 0, batchSize)
+		}
+	}
+
+	// The channel is closed, all routines have finished reading their files.
+	// Send the remaining batch to the server.
+	r.postWords(batch)
+	r.done <- true
+}
+
 // Run starts the ReaderWorker.
 //
 // It begins by reading files in parallel and
 // POSTing these to its configured destination.
 func (r *Reader) Run() {
 	log.Printf("Mapping %d files\n", len(r.files))
-	var wg sync.WaitGroup
 
+	// Start collecting and sending words
+	go r.collectWords(4096)
+
+	var wg sync.WaitGroup
 	// Read each file in parallel
 	for _, f := range r.files {
 		wg.Add(1)
@@ -99,6 +120,11 @@ func (r *Reader) Run() {
 		}(f)
 	}
 	wg.Wait()
+
+	// Tell collectWords we are done reading,
+	// wait for it to finish
+	close(r.wordQueue)
+	<-r.done
 }
 
 // NewReader creates an instance of ReaderWorker
@@ -109,7 +135,9 @@ func NewReader(destination string, files []string) *Reader {
 	}
 
 	return &Reader{
-		files:    files,
-		endpoint: url,
+		files:     files,
+		endpoint:  url,
+		wordQueue: make(chan string, len(files)),
+		done:      make(chan bool),
 	}
 }
